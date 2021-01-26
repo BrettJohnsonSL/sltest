ee59b7af2f38b1d42896891319d8ffd4b11849ef
Brett Johnson <brett@johnsonit.com.au>
2021-01-27 00:57:15 +1000

    moving to-do description to child row and integrating item editing functionality

--- sltest.py
+++ sltest.py
@@ -19,8 +19,12 @@ def send_js(path):
 	return send_from_directory('js/', path)
 
 @app.route('/css/<path:path>')
-def sendacss_(path):
+def send_css(path):
 	return send_from_directory('css/', path)
+
+@app.route('/img/<path:path>')
+def send_img(path):
+	return send_from_directory('img/', path)
 
 @app.route('/api/list_todo')
 def list_todo():
